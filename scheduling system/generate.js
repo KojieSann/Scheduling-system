82384fcdc2173cc79eaddf98e97dfe40e6e34dfa
KojieSann <johnlester.quinagutan@gmail.com>
2024-04-09 11:13:39 +0800

    kaantokkk

--- scheduling system/generate.js
+++ scheduling system/generate.js
@@ -24,6 +24,43 @@ document.addEventListener("click", (e) => {
     dropdownTable.classList.remove("activeShow");
   }
 });
+
+// select all for table
+const selectAll = document.getElementById("selectAll");
+const select = document.getElementsByClassName("select");
+selectAll.onclick = () => {
+  if (selectAll.checked == true) {
+    for (let i = 0; i < select.length; i++) {
+      select[i].checked = true;
+      document.querySelector(".table-nav").style.display = "block";
+    }
+  } else {
+    for (let i = 0; i < select.length; i++) {
+      select[i].checked = false;
+      document.querySelector(".table-nav").style.display = "none";
+    }
+  }
+};
+// pagination
+var pr = document.querySelector(".paginate.left");
+var pl = document.querySelector(".paginate.right");
+
+pr.onclick = slide.bind(this, -1);
+pl.onclick = slide.bind(this, 1);
+
+var index = 0,
+  total = 10;
+
+function slide(offset) {
+  index = Math.min(Math.max(index + offset, 0), total - 1);
+
+  document.querySelector(".counter").innerHTML = index + 1 + " / " + total;
+
+  pr.setAttribute("data-state", index === 0 ? "disabled" : "");
+  pl.setAttribute("data-state", index === total - 1 ? "disabled" : "");
+}
+
+slide(0);
 // modal for subject
 // to make it draggable hehe
 // const modalSubject = document.querySelector(".modal-content-subject"),
